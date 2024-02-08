fb6e8308abfe23cc05ed2163f49b4ae2504e6506
Lunic Petru <lunicv00@gmail.com>
2024-02-08 17:28:53 +0200

    Build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,12 @@
 import {Button} from "@radix-ui/themes";
+import Link from "next/link";
 
 
 export default async function Page() {
 
  return (
   <div>
-    <Button variant="solid">New Issue</Button>
+    <Button variant="solid"><Link href="/issues/new">New Issue</Link></Button>
   </div>
  );
 };
